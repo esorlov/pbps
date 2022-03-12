67f6eb31b3dfb4356d69bb6d261f9066afac3912
Egor S. Orlov <Egor.Orlov@avalon.ru>
2022-03-12 13:18:37 +0300

    BASIC Auth Example

--- 02.simple/HTTPSimple.c
+++ 02.simple/HTTPSimple.c
@@ -28,6 +28,8 @@ starts the server at port 10000 with ROOT as /home/shadyabhi
 
 #define CONNMAX 1000
 #define BYTES 1024
+#define AUTH_DATA "auth.html"
+
 
 char *ROOT;
 int listenfd, clients[CONNMAX];
@@ -136,12 +138,41 @@ void startServer(char *port)
 	}
 }
 
+// get file
+void get_req_resource(const char* path, int client, char* auth_data) 
+{
+	char data_to_send[BYTES];
+	int fd, bytes_read;
+	if (strncmp(&path[strlen(path)-strlen(AUTH_DATA)], AUTH_DATA, strlen(AUTH_DATA))==0)
+	{
+		printf("Auth needed...\n");
+		if (auth_data == NULL)
+		{
+			send(client, "HTTP/1.0 401 Unauthorized\n", 26, 0);
+			send(client, "WWW-Authenticate: Basic realm=\"Realm\"\n",38,0);
+			return;
+		} else
+			printf("Got Auth...%s\n", auth_data);
+	}
+	printf("file: %s\n", path);
+	printf("============================================\n");
+	printf("\n");
+	if ( (fd=open(path, O_RDONLY))!=-1 )    //FILE FOUND
+	{
+		send(client, "HTTP/1.0 200 OK\n\n", 17, 0);
+		while ( (bytes_read=read(fd, data_to_send, BYTES))>0 )
+			write (client, data_to_send, bytes_read);
+		close(fd);
+	}
+	else    write(client, "HTTP/1.0 404 Not Found\n", 23); //FILE NOT FOUND
+}
+
 //client connection
 void respond(int n)
 {
-	char mesg[99999], *reqline[3], data_to_send[BYTES], path[99999];
-	int rcvd, fd, bytes_read;
-
+	char mesg[99999], *reqline[3], path[99999];
+	int rcvd;
+	char *auth_data, *line, *save_ptr;
 	memset( (void*)mesg, (int)'\0', 99999 );
 
 	rcvd=recv(clients[n], mesg, 99999, 0);
@@ -149,15 +180,15 @@ void respond(int n)
 	if (rcvd<0)    // receive error
 		fprintf(stderr,("recv() error\n"));
 	else if (rcvd==0)    // receive socket closed
-		fprintf(stderr,"Client disconnected upexpectedly.\n");
+		fprintf(stderr,"Client disconnected unexpectedly.\n");
 	else    // message received
 	{
 		printf("%s", mesg);
-		reqline[0] = strtok (mesg, " \t\n");
+		reqline[0] = strtok_r (mesg, " \t\n", &save_ptr);
 		if ( strncmp(reqline[0], "GET\0", 4)==0 )
 		{
-			reqline[1] = strtok (NULL, " \t");
-			reqline[2] = strtok (NULL, " \t\n");
+			reqline[1] = strtok_r (NULL, " \t", &save_ptr);
+			reqline[2] = strtok_r (NULL, " \t\n", &save_ptr);
 			if ( strncmp( reqline[2], "HTTP/1.0", 8)!=0 && strncmp( reqline[2], "HTTP/1.1", 8)!=0 )
 			{
 				write(clients[n], "HTTP/1.0 400 Bad Request\n", 25);
@@ -166,20 +197,19 @@ void respond(int n)
 			{
 				if ( strncmp(reqline[1], "/\0", 2)==0 )
 					reqline[1] = "/index.html";        //Because if no file is specified, index.html will be opened by default (like it happens in APACHE...
-
+				line = strtok_r (NULL, "\r\n", &save_ptr);
+				while (line )
+				{
+					//printf("Next token: %s\n", line);
+					auth_data = strstr(line, "Authorization");
+					if (auth_data) break;
+					line = strtok_r(NULL, "\r\n", &save_ptr);
+				}
+				//printf ("AuthData: %s\n",auth_data);
 				strcpy(path, ROOT);
 				strcpy(&path[strlen(ROOT)], reqline[1]);
-				printf("file: %s\n", path);
-				printf("============================================\n");
-				printf("\n");
+				get_req_resource(path,clients[n],auth_data);
 
-				if ( (fd=open(path, O_RDONLY))!=-1 )    //FILE FOUND
-				{
-					send(clients[n], "HTTP/1.0 200 OK\n\n", 17, 0);
-					while ( (bytes_read=read(fd, data_to_send, BYTES))>0 )
-						write (clients[n], data_to_send, bytes_read);
-				}
-				else    write(clients[n], "HTTP/1.0 404 Not Found\n", 23); //FILE NOT FOUND
 			}
 		}
 	}
